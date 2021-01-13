14042bb1bfb6d3e6f640804d71ad5ed9ee9dcce7
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2021-01-13 21:40:33 +0100

    refactor: extract functionality into functions

--- miiLoader.js
+++ miiLoader.js
@@ -10,24 +10,50 @@
   const miiID = res[1];
   const clientID = res[2];
   const storageURL = `https%3A%2F%2Fstudio.mii.nintendo.com%2Fmiis%2F${miiID}%2Fedit%3Fclient_id%3D${clientID}`;
-  const load = confirm(
+
+  const setOrExtractData = confirm(
     "Press OK to load a Mii data file.\nPress Cancel to extract this Mii's Mii data file."
   );
-  if (load) {
-    const miiData = prompt('Please input the bytes for the Mii data file.');
-    localStorage.setItem(storageURL, miiData);
-    return;
+
+  if (setOrExtractData) {
+    setMiiData(storageURL);
+  } else {
+    const extractedMiiData = extractMiiData(storageURL);
+
+    downloadMiiData(extractedMiiData, miiID);
   }
-  var extractedMiiData = localStorage.getItem(storageURL);
-  if (!extractedMiiData) {
-    alert(
-      'The Mii data is not currently stored. Please make a change to this Mii, revert the change, and refresh the page to try again.'
-    );
-    return;
-  }
-  var d = document.createElement('a');
-  d.href = `data:attachment/text,${encodeURI(extractedMiiData)}`;
-  d.target = '_blank';
-  d.download = `${miiID}.txt`;
-  d.click();
 })();
+
+function setMiiData(storageURL) {
+  const miiData = prompt('Please input the bytes for the Mii data file.');
+
+  localStorage.setItem(storageURL, miiData);
+}
+
+function extractMiiData(storageURL) {
+  const extractedMiiData = localStorage.getItem(storageURL);
+
+  if (!extractedMiiData) {
+    const errorMessage =
+      'The Mii data is not currently stored. Please make a change to this Mii, revert the change, and refresh the page to try again.';
+
+    alert(errorMessage);
+    throw new Error(errorMessage);
+  }
+
+  return extractedMiiData;
+}
+
+function downloadMiiData(miiData, miiID) {
+  const encodedMiiData = encodeURI(miiData);
+
+  const downloadElement = document.createElement('a');
+
+  downloadElement.href = `data:attachment/text,${encodedMiiData}`;
+
+  downloadElement.target = '_blank';
+
+  downloadElement.download = `${miiID}.txt`;
+
+  downloadElement.click();
+}
